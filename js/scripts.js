ed45a80da45e696873a42838cb0cc979bfa7a065
heshashawky <heshamwebdeve@gmail.com>
2016-02-06 22:15:54 +0200

    ehabishak project by Hesham

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+var $;
+$(document).ready(function () {
+    "use strict";
+    /*owl carousel fire*/
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items : 4,
+        itemsDesktop : [1199, 3],
+        itemsDesktopSmall : [979, 3]
+    });
+
+    //Trigger MixitUp
+    $("#container").mixItUp();
+
+
+    //projects
+    $("#projects, #buildings").click(function () {
+        $(".spaces, .villas").fadeOut("fast");
+        $(".projects, .buildings").fadeToggle("slow");
+    });
+
+    $("#spaces, #villas").click(function () {
+        $(".projects, .buildings").fadeOut("fast");
+        $(".spaces, .villas").fadeToggle("slow");
+    });
+
+    /* loading page */
+    $(window).load(
+        function () {
+            $(".page-loader").fadeOut(2000, function () {
+                $(this).parent().parent().fadeOut(2000);
+            });
+        }
+    );
+
+});
